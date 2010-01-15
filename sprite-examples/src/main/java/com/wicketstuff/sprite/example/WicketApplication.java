045c28bca762abb1293f545cf2a27150200f06bd
annathakumaran <ananthakumaran@gmail.com>
2010-01-15 09:57:32 -0800

    setting up the project

---
+++ sprite-examples/src/main/java/com/wicketstuff/sprite/example/WicketApplication.java
@@ -0,0 +1,27 @@
+package com.wicketstuff.sprite.example;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application. If you want to run this application without deploying, run the Start class.
+ * 
+ * @see com.wicketstuff.sprite.example.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{    
+    /**
+     * Constructor
+     */
+	public WicketApplication()
+	{
+	}
+	
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	public Class<HomePage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+}
