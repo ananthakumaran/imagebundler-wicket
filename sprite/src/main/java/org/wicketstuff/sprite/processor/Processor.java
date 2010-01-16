45031ce48ecf267cff0faa21186ecd3e70ccd05c
annathakumaran <ananthakumaran@gmail.com>
2010-01-16 14:19:42 -0800

    created a simple source file using processor

--- sprite/src/main/java/org/wicketstuff/sprite/processor/Processor.java
+++ sprite/src/main/java/org/wicketstuff/sprite/processor/Processor.java
@@ -2,30 +2,79 @@ package org.wicketstuff.sprite.processor;
 
 import static javax.lang.model.SourceVersion.RELEASE_6;
 
+import java.io.Writer;
 import java.util.Set;
-import java.util.logging.Logger;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic.Kind;
 
+import org.wicketstuff.sprite.ImageBundle;
 
-@SuppressWarnings("restriction")
-@SupportedAnnotationTypes({"org.wicketstuff.sprite.ImageBundle"})
+
+@SupportedAnnotationTypes( { "org.wicketstuff.sprite.ImageBundle" })
 @SupportedSourceVersion(RELEASE_6)
 public class Processor extends AbstractProcessor
 {
-	Logger logger = Logger.getLogger(getClass().getName());
-
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv)
 	{
-		if (!roundEnv.processingOver())
-	    processingEnv.getMessager().printMessage(Kind.MANDATORY_WARNING, "hello World");
-	return false; // Don't claim any annotations
+		getMessager().printMessage(Kind.NOTE, "processing started");
+		try
+		{
+			for (Element element : roundEnv.getElementsAnnotatedWith(ImageBundle.class))
+			{
+				// handle  interface only
+				if (element.getKind() == ElementKind.INTERFACE)
+				{
+					String genClassName = element.getSimpleName()+"Bundle";
+					
+					Writer writer = getFiler().createSourceFile(genClassName).openWriter();
+					writer.write("public class " + genClassName + "{}" );
+					writer.close();
+					
+					getMessager().printMessage(Kind.NOTE, genClassName + " created");
+				}
+				else
+				{
+					warnElementIsUnhadled(element);
+				}
+
+			}
+		}
+		catch (Exception e)
+		{
+
+		}
+		getMessager().printMessage(Kind.NOTE, "processing finished");
+		return true;
 	}
 
+	private void warnElementIsUnhadled(Element element)
+	{
+		getMessager().printMessage(Kind.WARNING, "Unhandled element " + element);
+	}
+
+	private Filer getFiler()
+	{
+		return processingEnv.getFiler();
+	}
+
+	private Messager getMessager()
+	{
+		return processingEnv.getMessager();
+	}
+
+	private Elements getElementsUtils()
+	{
+		return processingEnv.getElementUtils();
+	}
 }
