fc97cb11ff5d5a6aab844394fce48e64fca3693d
annathakumaran <ananthakumaran@gmail.com>
2010-01-17 20:09:56 -0800

    cleaning up

--- sprite-examples/src/main/java/com/wicketstuff/sprite/example/SampleImage.java
+++ sprite-examples/src/main/java/com/wicketstuff/sprite/example/SampleImage.java
@@ -7,8 +7,6 @@ import org.wicketstuff.sprite.ImageBundle;
 @ImageBundle
 public interface SampleImage
 {
-
-	public Image boldImage(String id , String fileName);
+	public Image boldImage(String a, String fileName);
 	public Image codeImage(String id , String fileName);
-	
 }
