5f72018de67eb52f5ae39bc900d1dd9f22e0fc33
annathakumaran <ananthakumaran@gmail.com>
2010-01-20 10:45:46 -0800

    file logger updated

--- imagebundler/src/main/java/org/wicketstuff/imagebundler/processor/Processor.java
+++ imagebundler/src/main/java/org/wicketstuff/imagebundler/processor/Processor.java
@@ -1,12 +1,12 @@
 package org.wicketstuff.imagebundler.processor;
 
 import static javax.lang.model.SourceVersion.RELEASE_6;
+import static org.wicketstuff.imagebundler.processor.CurrentEnv.getLogger;
 import static org.wicketstuff.imagebundler.processor.CurrentEnv.getMessager;
 
 import java.io.OutputStream;
-import java.io.PrintStream;
-import java.lang.reflect.Field;
 import java.util.Set;
+import java.util.logging.Level;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.ProcessingEnvironment;
@@ -48,32 +48,35 @@ public class Processor extends AbstractProcessor
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv)
 	{
 		CurrentEnv.getMessager().printMessage(Kind.NOTE, "ImageBundle processing started");
+		boolean writeLogs = false;
 		try
 		{
 			for (Element element : roundEnv.getElementsAnnotatedWith(ImageBundle.class))
 			{
-				try
+				writeLogs = true;
+				getMessager().printMessage(Kind.NOTE, element.getSimpleName());
+				// handle interface only
+				if (element.getKind() == ElementKind.INTERFACE)
 				{
-					getMessager().printMessage(Kind.NOTE, element.getSimpleName());
-					// handle interface only
-					if (element.getKind() == ElementKind.INTERFACE)
-					{
-						new BundleClassGenerator(element).generate();
-					}
-					else
-					{
-						warnElementIsUnhadled(element);
-					}
+					new BundleClassGenerator(element).generate();
 				}
-				finally
+				else
 				{
-					logLogsToTextFile();
+					warnElementIsUnhadled(element);
 				}
 			}
 		}
 		catch (Exception e)
 		{
-			logExceptionToTextFile(e);
+			getLogger().log(Level.SEVERE, "", e);
+			writeLogs = true;
+		}
+		finally
+		{
+			if (writeLogs)
+			{
+				logLogsToTextFile();
+			}
 		}
 		getMessager().printMessage(Kind.NOTE, "processing finished");
 		return true;
@@ -82,35 +85,6 @@ public class Processor extends AbstractProcessor
 	private void warnElementIsUnhadled(Element element)
 	{
 		getMessager().printMessage(Kind.WARNING, "Unhandled element " + element);
-	}
-
-	/** Logs <code>e</code> to <code>SOURCE_OUTPUT/ImageBundler-errors.txt</code> */
-	private void logExceptionToTextFile(Exception e)
-	{
-		try
-		{
-			FileObject fo = this.processingEnv.getFiler().createResource(
-					StandardLocation.SOURCE_OUTPUT, "", "ImageBundler-exception.txt");
-			OutputStream out = fo.openOutputStream();
-			e.printStackTrace(new PrintStream(out));
-			// Specifically for Eclipse's AbortCompilation exception which has a
-			// useless printStackTrace output
-			try
-			{
-				Field f = e.getClass().getField("problem");
-				Object problem = f.get(e);
-				out.write(problem.toString().getBytes());
-			}
-			catch (NoSuchFieldException nsfe)
-			{
-			}
-			out.close();
-		}
-		catch (Exception e2)
-		{
-			this.processingEnv.getMessager().printMessage(Kind.ERROR,
-					"Error writing out error message " + e2.getMessage());
-		}
 	}
 
 	private void logLogsToTextFile()
